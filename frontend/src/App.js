391f38ef5abed461c5edef34e786e1097b0ca8b4
Marcus Eisele <marcus.eisele@gmail.com>
2017-11-05 11:01:31 +0100

    MAINT: Add creating basic messages
    At the moment creation only happens locally but update the messageWindow.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -67,6 +67,34 @@ class ConnectButton extends React.Component {
     }
 }
 
+class UserList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const userList = this.props.users;
+        return (
+            <div className="Userlist">
+                {userList.map(function (user) {
+                    return React.createElement("p", {key: "user" + user}, user);
+                })}
+            </div>
+        );
+    }
+}
+
+function MessageWindow(props) {
+
+    return (
+        <div className={"MessageWindow"}>
+            {props.messages.map(function (msg) {
+                return React.createElement("p", {key: "msg" + msg.id}, msg.user + ": " + msg.text);
+            })}
+        </div>
+    );
+}
+
 class UserNameField extends React.Component {
 
     constructor(props) {
@@ -91,7 +119,8 @@ class UserNameField extends React.Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>Username:
-                    <input type="text" value={this.state.value} onChange={this.handleUserNameChange} disabled={isDisabled} />
+                    <input type="text" value={this.state.value} onChange={this.handleUserNameChange}
+                           disabled={isDisabled}/>
                 </label>
                 <input type="submit" value="Set"/>
             </form>
@@ -99,17 +128,74 @@ class UserNameField extends React.Component {
     }
 }
 
+class MessageLine extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {text: ""};
+        this.onSubmit = this.onSubmit.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        let message = {
+                id: ID(),
+                user: this.props.userName,
+                text: this.state.text
+            };
+        this.props.appendNewMessage(message)
+    }
+
+    handleTextChange(event) {
+        this.setState({text: event.target.value});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <label>Text:
+                    <input type="text" value={this.state.value} onChange={this.handleTextChange}/>
+                </label>
+                <input type="submit" value="Set"/>
+            </form>
+        );
+
+    }
+
+
+}
+
 class App extends React.Component {
 
     constructor(props) {
         super(props);
+        window.app = this;
         this.state = {
             isConnected: false,
-            userName: "My Name"
+            userName: "My Name",
+            users: ["Mo", "Moo", "Mooo"],
+            messages: [
+                {
+                    id: 1,
+                    user: "testUser",
+                    text: "testText124551 12545"
+                },
+                {
+                    id: 2,
+                    user: "Mo",
+                    text: "I am the one and only!"
+                }, {
+                    id: 3,
+                    user: "Moo",
+                    text: "Nah! You write all the bugs!"
+                }
+            ]
         };
 
         this.changeConnectState = this.changeConnectState.bind(this);
         this.changeUserName = this.changeUserName.bind(this);
+        this.appendNewMessage = this.appendNewMessage.bind(this);
     }
 
     changeUserName(userName) {
@@ -129,24 +215,43 @@ class App extends React.Component {
         }));
     }
 
+    appendNewMessage(message) {
+
+        this.setState(prevState => ({
+            messages: prevState.messages.concat(message)
+        }));
+    }
+
     render() {
         const connectState = this.state.isConnected;
         const userName = this.state.userName;
+        const users = this.state.users;
+        const messages = this.state.messages;
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
-                <p className="App-intro">
+                <div className="App-intro">
                     <Clock/>
-                    <UserNameField userName={userName} onUserNameChange={this.changeUserName} connectState={connectState}/>
+                    <UserNameField userName={userName} onUserNameChange={this.changeUserName}
+                                   connectState={connectState}/>
                     <Welcome name={userName}/>
                     <ConnectButton connectState={connectState} onConnectStateChange={this.changeConnectState}/>
-                </p>
+                    <UserList users={users}/>
+                    <MessageWindow messages={messages}/>
+                    <MessageLine appendNewMessage={this.appendNewMessage} userName={userName}/>
+                </div>
             </div>
         );
     }
 }
 
+var ID = function () {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
 
 export default App;
